3454b6a7d692ed83c130e0e638705ffe6c6be6b8
Mateo <163420138+devMathiuX@users.noreply.github.com>
2024-11-02 16:58:39 -0300

    commit
    cambie double por float

--- InventarioTienda.java
+++ InventarioTienda.java
@@ -1,20 +1,19 @@
-﻿package proyectokk;
 import java.util.Scanner;
 
-public class Inventario {
+public class InventarioTienda {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //Definimos los vectores y variables a utilizar, los vectores tienen una capacidad de 50 espacios, para almacenar 50 productos
         int[] codigos = new int[50];
         String[] nombres = new String[50];
-        double[] precios = new double[50];
+        float[] precios = new float[50];
         int[] cantidades = new int[50];
         int contador = 0;
         int opcion;
 
         do {//Se abre el menu para seleccionar las opciones concedidas
-        	System.out.println("---------------------------------------------");
+            System.out.println("---------------------------------------------");
             System.out.println("SELECCIONE UNA OPCIÓN:                      |");
             System.out.println("1- Agregar un nuevo producto                |");
             System.out.println("2- Actualizar stock de un producto          |");
@@ -54,8 +53,8 @@ public class Inventario {
         } while (opcion != 0);
     }
 
-    public static int agregarProducto(int[] codigos, String[] nombres, double[] precios, int[] cantidades, int contador, Scanner input) {
-        if (contador >= 50) {//En este metodo utilizamos utilizamos un if para controlar el vector en caso de que el usuario llegue a poner el maximo de productos 
+    public static int agregarProducto(int[] codigos, String[] nombres, float[] precios, int[] cantidades, int contador, Scanner input) {
+        if (contador >= 50) {//En este metodo utilizamos utilizamos un if para controlar el vector en caso de que el usuario llegue a poner el maximo de productos
             System.out.println("No se pueden agregar más productos.");
             return contador;
         }
@@ -72,7 +71,7 @@ public class Inventario {
         nombres[contador] = input.next();
 
         System.out.println("Introduce el precio del producto:"); // Le pedimos al usuario que introduzca el precio del producto (puede ser decimal)
-        double precio = input.nextDouble();
+        float precio = input.nextFloat();
         if (precio <= 0) { //Si el usuario ingresa un precio menor o igual a 0, sale un error
             System.out.println("El precio debe ser mayor a 0.");
             return contador;
@@ -81,7 +80,7 @@ public class Inventario {
         System.out.println("Introduce la cantidad en stock:"); // Le pedimos al usuario que ingrese la cantidad de productos en stock
         int cantidad = input.nextInt();
         if (cantidad < 0) { //Si el usuario ingresa una cantidad  negativa de stock,sale un error
-            System.out.println("La cantidad debe ser positiva."); 
+            System.out.println("La cantidad debe ser positiva.");
             return contador;
         }
 
@@ -89,8 +88,8 @@ public class Inventario {
         precios[contador] = precio;
         cantidades[contador] = cantidad;
 
-        contador++; //Si todas las anteriores comprobaciones salen bien, el producto se agrega con éxito 
-        System.out.println("Producto agregado con éxito."); 
+        contador++; //Si todas las anteriores comprobaciones salen bien, el producto se agrega con éxito
+        System.out.println("Producto agregado con éxito.");
         return contador;
     }
 
@@ -103,17 +102,17 @@ public class Inventario {
                 System.out.println("Introduce la nueva cantidad en stock:");
                 aux=input.nextInt();
                 if (aux > 0 ){ //En esta parte se nos ofrece la posibilidad de cambiar el stock del producto que seleccionemos, pero si la cantidad del stock elegido es negativa no se cambiara.
-                	cantidades[i] = aux;
+                    cantidades[i] = aux;
                     System.out.println("Stock actualizado.");
                 }
                 else {
-                	System.out.println("ERROR:El stock no puede ser negativo");
+                    System.out.println("ERROR:El stock no puede ser negativo");
                 }
                 return;
             }}
-        }
+    }
 
-    public static void mostrarProductos(int[] codigos, String[] nombres, double[] precios, int[] cantidades, int contador) {
+    public static void mostrarProductos(int[] codigos, String[] nombres, float[] precios, int[] cantidades, int contador) {
         if (contador == 0) { //Si no hay productos en el inventario se muestra la advertencia
             System.out.println("No hay productos en el inventario.");
             return;
@@ -123,8 +122,8 @@ public class Inventario {
         }
     }
 
-    public static void buscarProductoPorCodigo(int[] codigos, String[] nombres, double[] precios, int[] cantidades, Scanner input) {
-        System.out.println("Introduce el código del producto:"); // En este metodo se le pide al usuario el codigo de un producto,luego se lo busca y se lo muestra, si no es encontrado se muestra una advertencia 
+    public static void buscarProductoPorCodigo(int[] codigos, String[] nombres, float[] precios, int[] cantidades, Scanner input) {
+        System.out.println("Introduce el código del producto:"); // En este metodo se le pide al usuario el codigo de un producto,luego se lo busca y se lo muestra, si no es encontrado se muestra una advertencia
         int codigo = input.nextInt();
 
         for (int i = 0; i < codigos.length; i++) {
@@ -149,7 +148,7 @@ public class Inventario {
         }
     }
 
-    public static void mostrarProductoMasCaroYMasBarato(String[] nombres, double[] precios, int contador) {
+    public static void mostrarProductoMasCaroYMasBarato(String[] nombres, float[] precios, int contador) {
         if (contador == 0) { //En este metodo si no hay productos en el inventario se muestra un mensaje, si hay productos se muestra el precio del mas caro y del mas barato
             System.out.println("No hay productos en el inventario.");
             return;
@@ -166,6 +165,7 @@ public class Inventario {
         }
 
         System.out.println("Producto más caro: " + nombres[indiceCaro] + " con un precio de " + precios[indiceCaro]);
-        System.out.println("Producto más barato: " + nombres[indiceBarato] + " con un precio de " + precios[indiceBarato]);
+        System.out.println(
+                "Producto más barato: " + nombres[indiceBarato] + " con un precio de " + precios[indiceBarato]);
     }
 }
